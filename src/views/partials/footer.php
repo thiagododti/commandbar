26d3c14e52626098f0759ce156ee0a767b893a95
Thiago de Oliveira Dantas <t.dod@live.com>
2021-08-29 14:31:00 -0300

    assets

---
+++ src/views/partials/footer.php
@@ -0,0 +1,7 @@
+<hr />
+<p>Todos os direitos reservados</p>
+<script type="text/javascript" src="<?= $base; ?>/assets/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="<?= $base; ?>/assets/js/jquery-3.6.0.min.js"></script>
+</body>
+
+</html>
--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -1,1 +1,12 @@
-<h1>Header</h1>
+<!DOCTYPE html>
+
+<head>
+	<title>Meu Crud MVC</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?= $base; ?>/assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="<?= $base; ?>/assets/css/variaveis.css" />
+</head>
+
+<body>
+	<h2>Crud MVC</h2>
+	<hr />
