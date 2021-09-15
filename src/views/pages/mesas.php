08f74b458ed8fb91c9b27cdd90cd8dc415689233
Thiago de Oliveira Dantas <t.dod@live.com>
2021-09-15 15:28:27 -0300

    Update mesas.php

--- src/views/pages/mesas.php
+++ src/views/pages/mesas.php
@@ -2,74 +2,28 @@
 <div class="col-10 justify-content-center">
 
     <div class="row">
-        <a class="col h-100" href="#" style="background-color: aqua;">
-            <div class="col">MESA 1<br><br><br><br><br></div>
+        <a class="col" id="mesas" href="#" style="background-color: aqua;">
+            <div class="btn btn-outline-primary">MESA 1</div>
         </a>
-        <a class="col" href="#">
-            <div class="col">MESA 2</div>
+        <a class="col" id="mesas" href="#">
+            <div class="btn btn-outline-primary">MESA 2</div>
         </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 3</div>
+        <a class="col" id="mesas" href="#" style="background-color: aqua;">
+            <div class="btn btn-outline-primary">MESA 3</div>
         </a>
-        <a class="col" href="#">
-            <div class="col">MESA 4</div>
+        <a class="col" id="mesas" href="#">
+            <div class="btn btn-outline-primary">MESA 4</div>
         </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 5</div>
+        <a class="col" id="mesas" href="#" style="background-color: aqua;">
+            <div class="btn btn-outline-primary">MESA 5</div>
         </a>
-        <a class="col" href="#">
-            <div class="col">MESA 6</div>
+        <a class="col" id="mesas" href="#">
+            <div class="btn btn-outline-primary">MESA 6</div>
         </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 7</div>
-        </a>
+
     </div>
-    <div class="row">
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 1<br><br><br><br><br></div>
-        </a>
-        <a class="col" href="#">
-            <div class="col">MESA 2</div>
-        </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 3</div>
-        </a>
-        <a class="col" href="#">
-            <div class="col">MESA 4</div>
-        </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 5</div>
-        </a>
-        <a class="col" href="#">
-            <div class="col">MESA 6</div>
-        </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 7</div>
-        </a>
-    </div>
-    <div class="row">
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 1<br><br><br><br><br></div>
-        </a>
-        <a class="col" href="#">
-            <div class="col">MESA 2</div>
-        </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 3</div>
-        </a>
-        <a class="col" href="#">
-            <div class="col">MESA 4</div>
-        </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 5</div>
-        </a>
-        <a class="col" href="#">
-            <div class="col">MESA 6</div>
-        </a>
-        <a class="col" href="#" style="background-color: aqua;">
-            <div class="col">MESA 7</div>
-        </a>
-    </div>
+
+</div>
 
 
 
