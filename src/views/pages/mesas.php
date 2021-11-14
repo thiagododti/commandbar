66089dd8d904075ed91ce4560502d3642e66fdf1
Thiago de Oliveira Dantas <t.dod@live.com>
2021-11-14 12:45:10 -0300

    Update mesas.php

--- src/views/pages/mesas.php
+++ src/views/pages/mesas.php
@@ -14,159 +14,159 @@
         <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
             <h2>Mesa 1</h2>
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/2">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/3">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/4">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/5">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/6">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/7">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/8">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/9">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-    </div>
-    <div class="row h-25 justify-content-center">
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/10">
             Mesa
         </a>
     </div>
     <div class="row h-25 justify-content-center">
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/11">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/12">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/13">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/14">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/15">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/16">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/17">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/18">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/19">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-    </div>
-    <div class="row h-25 justify-content-center">
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
-            Mesa
-        </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/20">
             Mesa
         </a>
     </div>
     <div class="row h-25 justify-content-center">
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/21">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/22">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/23">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/24">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/25">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/26">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/27">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/28">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/29">
             Mesa
         </a>
-        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/1">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/30">
+            Mesa
+        </a>
+    </div>
+    <div class="row h-25 justify-content-center">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/31">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/32">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/33">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/34">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/35">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/36">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/37">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/38">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/39">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/40">
+            Mesa
+        </a>
+    </div>
+    <div class="row h-25 justify-content-center">
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/41">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/42">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/43">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/44">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/45">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/46">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/47">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/48">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/49">
+            Mesa
+        </a>
+        <a class="col-1 m-2 btn btn-outline-primary" href="<?= $base ?>/mesa/50">
             Mesa
         </a>
     </div>
